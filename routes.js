1e8d165a939ad2a0f50dd5ad9357a9ee74b86d03
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-04-02 20:14:38 +0300

    Add 2 projects to the main page.

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 module.exports = function(app){
+    let views = `${__dirname}/views`;
     //MAIN
-    app.get("/", (req, res)=>{res.sendFile(`${__dirname}/views/main/index.html`)});
-    app.get("/style", (req, res)=>{res.sendFile(`${__dirname}/views/main/index.css`)});
+    app.get("/", (req, res)=>{res.sendFile(`${views}/main/index.html`)});
+    app.get("/style", (req, res)=>{res.sendFile(`${views}/main/index.css`)});
 
     //WRITING
-    app.get("/writing/style", (req, res)=>{res.sendFile(`${__dirname}/views/writing/index.css`)});
-    app.get("/writing/touchscreens", (req, res)=>{res.sendFile(`${__dirname}/views/writing/touchscreens.html`)});
+    app.get("/writing/style", (req, res)=>{res.sendFile(`${views}/writing/index.css`)});
+    app.get("/writing/touchscreens", (req, res)=>{res.sendFile(`${views}/writing/touchscreens.html`)});
 
     //IMAGES
-    app.get("/images/touchscreen", (req, res)=>{res.sendFile(`${__dirname}/views/images/touchscreen.jpeg`)});
+    app.get("/images/touchscreen", (req, res)=>{res.sendFile(`${views}/images/touchscreen.jpeg`)});
+    app.get("/images/subline", (req, res)=>{res.sendFile(`${views}/images/subline.png`)});
+    app.get("/images/budgeteer", (req, res)=>{res.sendFile(`${views}/images/budgeteer.jpeg`)});
 }
