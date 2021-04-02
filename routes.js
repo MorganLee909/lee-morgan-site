7044c35d35292a9d2fc830f8043c4b6e127c0e6b
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-04-02 19:49:24 +0300

    Create page with touchscreen article. Create style for writing pages.

--- routes.js
+++ routes.js
@@ -1,4 +1,12 @@
 module.exports = function(app){
+    //MAIN
     app.get("/", (req, res)=>{res.sendFile(`${__dirname}/views/main/index.html`)});
     app.get("/style", (req, res)=>{res.sendFile(`${__dirname}/views/main/index.css`)});
+
+    //WRITING
+    app.get("/writing/style", (req, res)=>{res.sendFile(`${__dirname}/views/writing/index.css`)});
+    app.get("/writing/touchscreens", (req, res)=>{res.sendFile(`${__dirname}/views/writing/touchscreens.html`)});
+
+    //IMAGES
+    app.get("/images/touchscreen", (req, res)=>{res.sendFile(`${__dirname}/views/images/touchscreen.jpeg`)});
 }
