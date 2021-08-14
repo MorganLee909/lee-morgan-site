139d6cdc663580452597ec16e51e04f14e4fa36e
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-08-14 10:12:47 +0300

    Add location to gallery model. Index the location propery on gallery model.

--- models/gallery.js
+++ models/gallery.js
@@ -11,7 +11,13 @@ const GallerySchema = new mongoose.Schema({
         required: true
     },
     tags: [String],
-    images: [String]
+    images: [String],
+    location: {
+        type: {type: String},
+        coordinates: [],
+        required: false
+    }
 });
+GallerySchema.index({location: "2dsphere"});
 
 module.exports = mongoose.model("Gallery", GallerySchema);
