b0febd1bbe1bcdf653ad77c3291ec7df67e6745b
Lee Morgan <me@leemorgan.io>
2021-10-01 18:15:16 +0300

    Add frontend to create a new question.

--- controllers/learn.js
+++ controllers/learn.js
@@ -263,6 +263,7 @@ module.exports = {
     response: Question
     */
     createQuestion: function(req, res){
+        console.log(req.body);
         let question = new Question({
             name: req.body.name,
             title: req.body.title,
