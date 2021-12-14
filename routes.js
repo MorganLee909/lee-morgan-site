5fc9b92f26acfbe3c420264e1a1e415d5b6c0146
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-12-14 12:40:38 +0300

    Add favicon

--- routes.js
+++ routes.js
@@ -38,6 +38,7 @@ module.exports = function(app){
     app.get("/images/rumble", (req, res)=>{res.sendFile(`${views}/images/rumble.png`)});
     app.get("/images/covid", (req, res)=>{res.sendFile(`${views}/images/covid.jpeg`)});
     app.get("/images/currency", (req, res)=>res.sendFile(`${views}/images/currency.jpeg`));
+    app.get("/images/favicon", (req, res)=>res.sendFile(`${views}/images/favicon.ico`));
 
     //SUDOKU
     app.get("/sudoku", visit, (req, res)=>{res.sendFile(`${views}/sudoku/index.html`)});
