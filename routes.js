e3767bb94e0a9e8c677c12a95a665d0a50216846
Lee Morgan <me@leemorgan.io>
2022-05-25 10:37:04 +0300

    Remove visitor tracking. Remove covid shit. Remove some other dead parts of the website.

--- routes.js
+++ routes.js
@@ -1,29 +1,26 @@
 const gallery = require("./controllers/gallery.js");
 const learn = require("./controllers/learn.js");
 const blog = require("./controllers/blog.js");
-const covid = require("./controllers/covid.js");
 const currency = require("./controllers/currency.js");
-
-const visit = require("./middleware.js").visit;
 
 module.exports = function(app){
     let views = `${__dirname}/views`;
 
     //MAIN
-    app.get("/", visit, (req, res)=>{res.sendFile(`${views}/main/index.html`)});
+    app.get("/", (req, res)=>{res.sendFile(`${views}/main/index.html`)});
     app.get("/style", (req, res)=>{res.sendFile(`${views}/main/index.css`)});
 
     //RESUME
-    app.get("/resume", visit, (req, res)=>{res.sendFile(`${views}/resume/resume.html`)});
+    app.get("/resume", (req, res)=>{res.sendFile(`${views}/resume/resume.html`)});
     app.get("/resume/style", (req, res)=>{res.sendFile(`${views}/resume/resume.css`)});
 
     //BLOG
     app.get("/blog/style", (req, res)=>{res.sendFile(`${views}/blog/index.css`)});
-    app.get("/blog/new", visit, (req, res)=>{res.sendFile(`${views}/blog/new.html`)});
+    app.get("/blog/new", (req, res)=>{res.sendFile(`${views}/blog/new.html`)});
     app.post("/blog/new", blog.create);
     app.get("/blog/retrieve", blog.getBlogs);
     app.get("/blog/json/:id", blog.getBlog);
-    app.get("/blog/:id", visit, (req, res)=>{res.sendFile(`${views}/blog/index.html`)});
+    app.get("/blog/:id", (req, res)=>{res.sendFile(`${views}/blog/index.html`)});
 
     //IMAGES
     app.get("/images/subline", (req, res)=>{res.sendFile(`${views}/images/subline.png`)});
@@ -41,63 +38,55 @@ module.exports = function(app){
     app.get("/images/favicon", (req, res)=>res.sendFile(`${views}/images/favicon.ico`));
 
     //SUDOKU
-    app.get("/sudoku", visit, (req, res)=>{res.sendFile(`${views}/sudoku/index.html`)});
+    app.get("/sudoku", (req, res)=>{res.sendFile(`${views}/sudoku/index.html`)});
     app.get("/sudoku/style", (req, res)=>{res.sendFile(`${views}/sudoku/index.css`)});
     app.get("/sudoku/code", (req, res)=>{res.sendFile(`${views}/sudoku/index.js`)});
 
     //BIRTHDAY PARADOX
-    app.get("/birthdayparadox", visit,(req, res)=>{res.sendFile(`${views}/birthdayParadox/index.html`)}); 
+    app.get("/birthdayparadox",(req, res)=>{res.sendFile(`${views}/birthdayParadox/index.html`)}); 
     app.get("/birthdayparadox/style", (req, res)=>{res.sendFile(`${views}/birthdayParadox/index.css`)});
     app.get("/birthdayparadox/code", (req, res)=>{res.sendFile(`${views}/birthdayParadox/index.js`)});
 
     //GALLERY
     app.get("/gallery/style", (req, res)=>{res.sendFile(`${views}/gallery/index.css`)});
-    app.get("/gallery", visit, (req, res)=>{res.sendFile(`${views}/gallery/galleries.html`)});
+    app.get("/gallery", (req, res)=>{res.sendFile(`${views}/gallery/galleries.html`)});
     app.get("/gallery/new", (req, res)=>{res.sendFile(`${views}/gallery/new.html`)});
     app.post("/gallery/new", gallery.create);
     app.get("/gallery/retrieve", gallery.getGalleries);
-    app.get("/gallery/edit/:id", visit, (req, res)=>{res.sendFile(`${views}/gallery/editGallery.html`)});
+    app.get("/gallery/edit/:id", (req, res)=>{res.sendFile(`${views}/gallery/editGallery.html`)});
     app.post("/gallery/update/:id", gallery.updateGallery);
     app.get("/gallery/json/:id", gallery.getGallery);
-    app.get("/gallery/:id", visit, (req, res)=>{res.sendFile(`${views}/gallery/index.html`)});
+    app.get("/gallery/:id", (req, res)=>{res.sendFile(`${views}/gallery/index.html`)});
 
     //LEARN
     app.get("/learn/style", (req, res)=>{res.sendFile(`${views}/learn/index.css`)});
     
-    app.get("/learn/courses/new", visit, (req, res)=>{res.sendFile(`${views}/learn/newCourse.html`)});
+    app.get("/learn/courses/new", (req, res)=>{res.sendFile(`${views}/learn/newCourse.html`)});
     app.post("/learn/courses/new", learn.createCourse);
     app.get("/learn/courses/json", learn.getCourses);
-    app.get("/learn/courses/:id", visit, (req, res)=>{res.sendFile(`${views}/learn/course.html`)});
-    app.get("/learn", visit, (req, res)=>{res.sendFile(`${views}/learn/courses.html`)});
+    app.get("/learn/courses/:id", (req, res)=>{res.sendFile(`${views}/learn/course.html`)});
+    app.get("/learn", (req, res)=>{res.sendFile(`${views}/learn/courses.html`)});
     app.get("/learn/answers/new/", (req, res)=>{res.sendFile(`${views}/learn/newAnswer.html`)});
 
-    app.get("/learn/lectures/new", visit, (req, res)=>{res.sendFile(`${views}/learn/newLecture.html`)});
+    app.get("/learn/lectures/new", (req, res)=>{res.sendFile(`${views}/learn/newLecture.html`)});
     app.post("/learn/lectures/new", learn.createLecture);
-    app.get("/learn/lectures/edit/:id", visit, (req, res)=>{res.sendFile(`${views}/learn/editLecture.html`)});
+    app.get("/learn/lectures/edit/:id", (req, res)=>{res.sendFile(`${views}/learn/editLecture.html`)});
     app.post("/learn/lectures/edit/:id", learn.updateLecture);
     app.delete("/learn/lectures/:id", learn.removeLecture);
     app.get("/learn/lectures/json/:id", learn.getLectures);
-    app.get("/learn/lectures/:id", visit, (req, res)=>{res.sendFile(`${views}/learn/lecture.html`)});
+    app.get("/learn/lectures/:id", (req, res)=>{res.sendFile(`${views}/learn/lecture.html`)});
     app.get("/learn/lectures/json/one/:id", learn.getLecture);
 
     app.post("/learn/questions/create", learn.createQuestion);
     app.post("/learn/answers/create", learn.createAnswer);
 
-    app.post("/htmltest", visit, (req, res)=>{res.send(`Hi ${req.body.name} from ${req.body.place}`)});
-
-    //COVID
-    app.get("/covid", visit, (req, res)=>{res.sendFile(`${views}/covid/covid.html`)});
-    app.get("/covid/style", (req, res)=>{res.sendFile(`${views}/covid/covid.css`)});
-    app.post("/covid", visit, covid.data);
+    app.post("/htmltest", (req, res)=>{res.send(`Hi ${req.body.name} from ${req.body.place}`)});
 
     //CURRENCY
     app.get("/currency/new", (req, res)=>{res.sendFile(`${views}/currency/new.html`)});
     
     app.post("/currency", currency.create);
     app.get("/currency/data", currency.getData);
-
-    //UKRAINE
-    app.get("/ukraine", (req, res)=>res.sendFile(`${views}/ukraine/ukraine.html`));
 
     //CONTENT
     app.get("/thumbNails/*", (req, res)=>{res.sendFile(`${__dirname}${req.url}`)});
